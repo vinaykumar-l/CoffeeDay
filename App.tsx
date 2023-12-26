3b9563e6018b8c70757c5dc3f9a266ade5cdf966
VINAY KUMAR <122248841+vinaykumar-l@users.noreply.github.com>
2023-12-26 21:39:54 +0530

    development commit

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {AppRegistry} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import DetailsScreen from './src/screens/DetailsScreen';
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,34 +1,56 @@
 import {StyleSheet, Text, View, ScrollView} from 'react-native';
 import React, {useState} from 'react';
 import {useStore} from '../store/store';
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
 
-const getCategoriesFromData = (data: any) => {
-  let temp: any = {};
-  for (let i = 0; i < data.length; i++) {
-    if (temp[data[i].name] == undefined) {
-      temp[data[i].name] = 1;
-    } else {
-      temp[data[i].name]++;
-    }
+interface CoffeeShopItem {
+  name: string;
+  // ... other properties
+}
+
+// Function to return array of sorted categories received from the data
+const getCategoriesFromData = (data: CoffeeShopItem[]): string[] => {
+  const temp: {[category: string]: number} = {};
+
+  for (const item of data) {
+    const categoryName: string = item.name;
+    temp[categoryName] = (temp[categoryName] || 0) + 1;
   }
 
-  let categories = Object.keys(temp);
-  categories.unshift('All');
+  const categories: string[] = ['All', ...Object.keys(temp)];
   return categories;
 };
+
+// Function to return coffee data based on category
+const getCoffee = (category: string, data: any) => {
+  if (category === 'All') {
+    return data;
+  } else {
+    let coffeeList = data.filter((item: any) => item.name == category);
+    return coffeeList;
+  }
+};
+
+// Bootom Tabbar Height
+const tabbarHeight = useBottomTabBarHeight();
 
 const HomeScreen = () => {
   const CoffeeList = useStore((state: any) => state.CoffeeList);
   const BeanList = useStore((state: any) => state.BeanList);
 
-  const [categories, setCategories] = useState([undefined]);
+  const [categories, setCategories] = useState(
+    getCategoriesFromData(CoffeeList),
+  );
   const [searchText, setSearchText] = useState(undefined);
   const [categoryIndex, setCategoryIndex] = useState({
     index: 0,
     category: categories[0],
   });
-  const [sortedCoffee, setSortedCoffee] = useState(undefined);
+  const [sortedCoffee, setSortedCoffee] = useState(
+    getCoffee(categoryIndex.category, CoffeeList),
+  );
 
+  // Home-Page JSX starts here
   return <ScrollView></ScrollView>;
 };
 
